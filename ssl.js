93c0f4b7e9d5ded5695450f8e41869d98378b54f
Balás Botond Barna <buduboti@yahoo.com>
2020-10-06 00:44:29 +0000

    ssl

---
+++ ssl.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 1337;
+
+app.use(express.static('ssl', { dotfiles: 'allow' } ));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(PORT, () => {
+    console.log(`App is listening on port: ${ PORT }`);
+})
