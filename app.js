7bcb5094db063dd818b512385c77c30ae55823cf
Balás Botond Barna <buduboti@yahoo.com>
2020-10-06 00:47:16 +0000

    ssl app

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
-const express = require('express')
-const app = express()
-const PORT = 1337
+//  Dependencies
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const express = require('express');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const app = express();
 
-app.listen(port, () => {
-    console.log(`App is listening on port: ${ PORT }`)
-})
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/buduboti.xyz/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/buduboti.xyz/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/buduboti.xyz/chain.pem', 'utf8');
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca
+};
+
+app.use((req, res) => {
+  res.send('Hello there !');
+});
+
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+  console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+  console.log('HTTPS Server running on port 443');
+});
